76e025e5ea39ddfa3b1b0ef928a2e22e42723ea5
avremi89 <avremi89@gmail.com>
2021-08-04 17:03:52 +0300

    a
    a

--- src/components/ReactOff.js
+++ src/components/ReactOff.js
@@ -5,7 +5,7 @@
 <
 script src = "https://unpkg.com/react@17/umd/react.development.js"
 crossorigin > < /script> <
-    script src = "https://unpkg.com/react-dom@17/umd/react-dom.development.js"
+script src = "https://unpkg.com/react-dom@17/umd/react-dom.development.js"
 crossorigin > < /script>
 
 <!-- Load our React component. -->
@@ -16,21 +16,24 @@ script src = "like_button.js" > < /script>
 /body>
 
 ===
-=== === === === =
+===
+=== === === =
 // ... the starter code you pasted ...
 
 const domContainer = document.querySelector('#like_button_container');
 ReactDOM.render(e(LikeButton), domContainer);
 
 ===
-=== === === === = <
+===
+=== === === = <
 script src = "https://unpkg.com/react@17/umd/react.production.min.js"
 crossorigin > < /script> <
-    script src = "https://unpkg.com/react-dom@17/umd/react-dom.production.min.js"
+script src = "https://unpkg.com/react-dom@17/umd/react-dom.production.min.js"
 crossorigin > < /script>
 
 ===
-=== === === ===
+===
+=== === ===
 const e = React.createElement;
 
 // Display a "Like" <button>
@@ -42,8 +45,23 @@ return e(
 // Display a "Like" <button>
 return ( <
     button onClick = {
-        () => this.setState({ liked: true }) } >
+        () => this.setState({ liked: true })
+    } >
     Like <
     /button>
 ); ===
-=== === === === =
+===
+=== === === =
+
+
+// Quickly Try JSX
+// The quickest way to try JSX in your project is to add this <script> tag to your page:
+
+<
+script src = "https://unpkg.com/babel-standalone@6/babel.min.js" > < /script>
+
+
+// Run JSX Preprocessor
+// Create a folder called src and run this terminal command:
+
+npx babel--watch src--out - dir.--presets react - app / prod
